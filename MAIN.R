94610672a1f25bf6e6ac88f193403b3591d62534
dvanavermaete <david.vanavermaete@ilvo.vlaanderen.be>
2025-02-04 12:09:15 +0100

    Update: GAM models moved to GAM directory. MAIN file to run the code.

---
+++ MAIN.R
@@ -0,0 +1,21 @@
+rm(list=ls()) #clean up memory before start
+options(dplyr.summarise.inform = FALSE) #Do not print info from dplyr package 
+# ---------------------------------------------
+#User parameters
+# ---------------------------------------------
+#path to data file
+path='data/EMODnet/EMD_seafloorbaselines_EEA_waters_filtered_20230317.csv'
+
+#which dataset do you want to import? Choose amongst EMOD, BITS, 
+#BTS, DYFS, EVHOE, IE-IGFS, NS-IBTS, SCOWCGFS
+type='EMOD'
+
+# ---------------------------------------------
+# Make the Overview Excel
+# ---------------------------------------------
+source("Litter_summary/Calculate_Litter_Parameters.R")
+
+# ---------------------------------------------
+# Make the GAM models
+# ---------------------------------------------
+source('GAM/runLitter.R')
